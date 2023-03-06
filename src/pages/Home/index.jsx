0e63e5ebafae6d809f8b4b1b64ee511cec53f832
Khoi Tran <tranquockhoi1999@gmail.com>
2023-03-06 13:34:51 +0700

    scroll-bar, header potision fixed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,9 @@
+import classNames from 'classnames/bind';
+import style from './Home.module.scss';
+
+const cx = classNames.bind(style);
 function Home({ children }) {
-    return <div className="HomePage">{children}</div>;
+    return <div className={cx('HomePage')}>{children}</div>;
 }
 
 export default Home;
