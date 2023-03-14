b1c946f4c9d80a83a0e873aa279a1a32ffce16b4
Khoi Tran <tranquockhoi1999@gmail.com>
2023-03-14 16:50:25 +0700

    Finish UserIteractive

--- src/components/Foryou/ListContent/VideoContent/UserInteractive/UserInteractive.jsx
+++ src/components/Foryou/ListContent/VideoContent/UserInteractive/UserInteractive.jsx
@@ -3,10 +3,89 @@ import Tippy from '@tippyjs/react/headless';
 import 'animate.css';
 import style from './UserInteractive.module.scss';
 import classNames from 'classnames/bind';
+import { Menu } from '../../../../DetailComponent';
 
 const cx = classNames.bind(style);
 const UserInteractive = () => {
+    const MenuShare = [
+        {
+            icon: <i className="fa-solid fa-code"></i>,
+            title: 'Embed',
+        },
+        {
+            icon: <i className="fa-solid fa-paper-plane"></i>,
+            title: 'Send to friends',
+        },
+        {
+            icon: <i className="fa-brands fa-facebook"></i>,
+            title: 'Share to Facebook',
+        },
+        {
+            icon: <i className="fa-brands fa-whatsapp"></i>,
+            title: 'Share to WhatsApp',
+        },
+        {
+            icon: <i className="fa-solid fa-link"></i>,
+            title: 'Copy link',
+        },
+        {
+            dropDown: <i class="fa-solid fa-angle-down"></i>,
+            children: {
+                list: [
+                    {
+                        icon: <i className="fa-solid fa-code"></i>,
+                        title: 'Embed',
+                    },
+                    {
+                        icon: <i className="fa-solid fa-paper-plane"></i>,
+                        title: 'Send to friends',
+                    },
+                    {
+                        icon: <i className="fa-brands fa-facebook"></i>,
+                        title: 'Share to Facebook',
+                    },
+                    {
+                        icon: <i className="fa-brands fa-whatsapp"></i>,
+                        title: 'Share to WhatsApp',
+                    },
+                    {
+                        icon: <i className="fa-solid fa-link"></i>,
+                        title: 'Copy link',
+                    },
+                    {
+                        icon: <i className="fa-brands fa-twitter"></i>,
+                        title: 'Share to Twitter',
+                    },
+                    {
+                        icon: <i className="fa-brands fa-linkedin-in"></i>,
+                        title: 'Share to Linkedln',
+                    },
+                    {
+                        icon: <i className="fa-brands fa-reddit-alien"></i>,
+                        title: 'Share to Reddit',
+                    },
+                    {
+                        icon: <i className="fa-brands fa-telegram"></i>,
+                        title: 'Share to Telegram',
+                    },
+                    {
+                        icon: <i class="fa-solid fa-envelope"></i>,
+                        title: 'Share to Email',
+                    },
+                    {
+                        icon: <i class="fa-brands fa-line"></i>,
+                        title: 'Share to Line',
+                    },
+                    {
+                        icon: <i class="fa-brands fa-pinterest"></i>,
+                        title: 'Share to Pinterest',
+                    },
+                ],
+            },
+        },
+    ];
     const [heart, setHeart] = useState(false);
+    const [isResetMenu, setIsResetMenu] = useState(false);
 
     const handleHeartActive = () => {
         if (heart) {
@@ -23,7 +102,7 @@ const UserInteractive = () => {
                 <i
                     className={cx('fa-solid fa-heart', {
                         'heart-active': heart,
-                        'animate__animated animate__heartBeat': heart,
+                        'animate__animated animate__bounceIn': heart,
                     })}
                 ></i>
             </div>
@@ -32,10 +111,25 @@ const UserInteractive = () => {
                 <i className="fa-solid fa-comment-dots"></i>
             </div>
             <p>7789</p>
-            <div className={cx('icon-wrapper')}>
-                <i className="fa-solid fa-share"></i>
-            </div>
-            <p>17.8k</p>
+            <Tippy
+                delay={[0, 500]}
+                interactive
+                placement="top-start"
+                render={(attrs) => <Menu data={MenuShare} isResetMenu={isResetMenu} />}
+                onHide={() => {
+                    setIsResetMenu(true);
+                }}
+                onShow={() => {
+                    setIsResetMenu(false);
+                }}
+            >
+                <div className={cx('wrapper')}>
+                    <div className={cx('icon-wrapper')}>
+                        <i className="fa-solid fa-share"></i>
+                    </div>
+                    <p>17.8k</p>
+                </div>
+            </Tippy>
         </div>
     );
 };
