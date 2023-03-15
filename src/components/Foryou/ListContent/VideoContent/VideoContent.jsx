76397d416bfd3e8682765928d00c25231191281e
Khoi Tran <tranquockhoi1999@gmail.com>
2023-03-15 08:50:42 +0700

    Update scroll to autoplay/pause, responsive

--- src/components/Foryou/ListContent/VideoContent/VideoContent.jsx
+++ src/components/Foryou/ListContent/VideoContent/VideoContent.jsx
@@ -19,7 +19,7 @@ const VideoContent = ({ dataVideo }) => {
     const options = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.3,
+        threshold: 0.9,
     };
     const isVisible = useElementOnScreen(options, videoRef);
 
