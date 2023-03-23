9d258200d848a22a338cb0f7abb8e56ebc579576
Khoi Tran <tranquockhoi1999@gmail.com>
2023-03-23 10:29:00 +0700

    Logical Message

--- src/components/Messages/ChatAccountList/ChatAccountList.jsx
+++ src/components/Messages/ChatAccountList/ChatAccountList.jsx
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import useFireStore from '../../../hooks/useFireStore';
-import { UserChatListSelector } from '../../../redux/selector';
+import { CurrentRoomsSelector, UserChatListSelector } from '../../../redux/selector';
 import AccountItem from './AccountItem/AccountItem';
 import style from './ChatAccountList.module.scss';
 import MessagesOfRoomSlice from '../ChatBox/BodyChatBox/MessagesOfRoomSlice';
@@ -11,6 +11,7 @@ const cx = classNames.bind(style);
 const ChatAccountList = () => {
     const dispatch = useDispatch();
     const userChatList = useSelector(UserChatListSelector);
+    const curRoom = useSelector(CurrentRoomsSelector);
 
     const messages = useFireStore('messages', '', 'createdAt', 'asc');
 
@@ -21,16 +22,22 @@ const ChatAccountList = () => {
     return (
         <div className={cx('accountList')}>
             <ul>
-                {userChatList.map((val) => (
-                    <AccountItem
-                        key={val.uid}
-                        messages={messages}
-                        uid={val.uid}
-                        avatar={val.photoURL}
-                        name={val.displayName}
-                        user={val}
-                    />
-                ))}
+                {curRoom.length > 0 ? (
+                    <>
+                        {userChatList.map((val) => (
+                            <AccountItem
+                                key={val.uid}
+                                messages={messages}
+                                uid={val.uid}
+                                avatar={val.photoURL}
+                                name={val.displayName}
+                                user={val}
+                            />
+                        ))}
+                    </>
+                ) : (
+                    <p className={cx('empty')}>No messages yet</p>
+                )}
             </ul>
         </div>
     );
